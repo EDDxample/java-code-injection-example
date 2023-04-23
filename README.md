7ca8b417cc2cf47399cfca21717b667f69743b74
EDDxample <EDDxample@gmail.com>
2023-04-23 00:46:28 +0000

    Add agent2 + minecraft thumbnail

--- README.md
+++ README.md
@@ -22,17 +22,20 @@ java sample/Injector <PID> [agent0|agent1|agent2] <path to agent.jar>
 ```
 
 
-## Goals (a.k.a. Agents)
+## Agents
 - [x] `agent0`: Prints something into the target process.
 - [x] `agent1`: Calls a private function from the target process.
-- [ ] `agent2`: Executes custom code from a Minecraft process.
+- [x] `agent2`: Executes custom code from a Minecraft process.
 
 
 ## Devcontainer
 I hate managing multiple java versions, so I added a `.devcontainer` config in case you want to use the same environment as me.
+
+(Note: If you run the injector from the container, it wont get access to the Minecraft PID)
 
 https://code.visualstudio.com/docs/devcontainers/containers
 
 
 ## Sources
 - [What is a Java Agent](https://youtu.be/ShSjzru4kZA)
+- [Mojmap Viewer](https://minidigger.github.io/MiniMappingViewer/#/mojang/client/1.19.4/)
--- sample/Agent.java
+++ sample/Agent.java
@@ -7,7 +7,7 @@ import java.lang.reflect.Method;
 public class Agent {
 
     /*
-     * Decides which agent to run.
+     * Agent's main function, decides which agent to run.
      */
     public static void agentmain(String args, Instrumentation inst) throws Exception {
         switch (args) {
@@ -35,22 +35,72 @@ public class Agent {
      * Calls a private function from the target process.
      */
     public static void agent1(Instrumentation inst) throws Exception {
-        Class<?> clazz = Class.forName("sample.Target");
+        Class<?> Target = Class.forName("sample.Target");
 
         // access Target's `private static String secretField`
-        Field f = clazz.getDeclaredField("secretField");
-        f.setAccessible(true);
-        System.out.println(f.get(clazz));
+        Object value = getField(Target, "secretField").get(Target);
+        System.out.println(value);
 
         // access Target's `private static void secretFunction()`
-        Method m = clazz.getDeclaredMethod("secretFunction");
-        m.setAccessible(true);
-        m.invoke(clazz);
+        getMethod(Target, "secretFunction").invoke(Target);
     }
 
     /*
-     * Executes custom code from a Minecraft process.
+     * Executes custom code from a Minecraft 1.19.4 process.
      */
-    public static void agent2(Instrumentation inst) {
+    public static void agent2(Instrumentation inst) throws Exception {
+        Class<?> Minecraft = Class.forName(translate("net.minecraft.client.Minecraft"));
+        Class<?> Component = Class.forName(translate("net.minecraft.network.chat.Component"));
+
+        // Component.literal("text")
+        Method literal = getMethod(Component, translate("literal"), String.class);
+        Object component = literal.invoke(Component, "*hacker voice* I'm in.");
+
+        // Minecraft.instance
+        Object instance = getField(Minecraft, translate("instance")).get(Minecraft);
+
+        // Minecraft.instance.player
+        Object player = getField(Minecraft, translate("player")).get(instance);
+
+        // Minecraft.instance.player.sendSystemMessage(Component.literal("text"));
+        Method sendSystemMessage = getMethod(player.getClass(), translate("sendSystemMessage"), Component);
+        sendSystemMessage.invoke(player, component);
+    }
+
+    /*
+     * Dirty way to translate from Mojang Mappings to the Obfuscated version.
+     */
+    public static String translate(String name) {
+        return switch (name) {
+            case "net.minecraft.network.chat.Component" -> "tj";
+            case "literal" -> "b";
+
+            case "net.minecraft.client.Minecraft" -> "emh";
+            case "instance" -> "F";
+            case "player" -> "t";
+
+            case "net.minecraft.client.player.LocalPlayer" -> "fhk";
+            case "sendSystemMessage" -> "a";
+
+            default -> name;
+        };
+    }
+
+    /*
+     * Get a method by name and force it to be public.
+     */
+    public static Method getMethod(Class<?> base, String name, Class<?>... params) throws Exception {
+        Method m = base.getDeclaredMethod(name, params);
+        m.setAccessible(true);
+        return m;
+    }
+
+    /*
+     * Get a field by name and force it to be public.
+     */
+    public static Field getField(Class<?> base, String name) throws Exception {
+        Field f = base.getDeclaredField(name);
+        f.setAccessible(true);
+        return f;
     }
 }
